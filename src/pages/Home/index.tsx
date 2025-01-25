c498465392986a9fd83bdd3519cac01b4a8da396
Afrijal Dzuhri <hi@afrijal.dev>
2025-01-25 23:53:50 +0700

    fix: home view

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen lg:w-[800px] text-center space-y-14">
+          <h1 className="text-6xl text-blue-200 leading-tight">Financial Stability Starts with Fixed Income Solutions</h1>
+          <p className="text-lg text-blue-100">Harness the power of funding rates and blockchain technology to generate predictable, steady income streams. Our solutions are designed to bring stability to your crypto investments.</p>
+      </div>
+    </>
+  );
+};
