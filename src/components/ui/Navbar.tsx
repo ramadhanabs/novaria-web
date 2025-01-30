111bcd3eb82e6824b5ac0c341bfb6d21a72563a8
ijul <hi@afrijal.dev>
2025-01-30 13:12:49 +0700

    fix: navbar and home responsive

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,18 +3,15 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export const Navbar = () => {
   return (
     <nav className="h-[100px] fixed w-full z-10 flex items-center bg-black text-white">
-      <div className="container mx-auto flex justify-between items-center px-4">
-        <ul className="flex space-x-6">
+      <div className="container mx-auto flex lg:justify-between justify-end items-center px-4">
+        <ul className="lg:flex hidden space-x-6">
           <li>
             <a href="/" className="text-white hover:text-main font-bold">
               Home
             </a>
           </li>
           <li>
-            <a
-              href="/contact"
-              className="text-white hover:text-main font-bold"
-            >
+            <a href="/contact" className="text-white hover:text-main font-bold">
               Deposit
             </a>
           </li>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,27 @@
 export const Home = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen lg:w-[800px] text-center space-y-14">
-          <h1 className="text-6xl text-blue-200 leading-tight">Financial Stability Starts with Fixed Income Solutions</h1>
-          <p className="text-lg text-blue-100">Harness the power of funding rates and blockchain technology to generate predictable, steady income streams. Our solutions are designed to bring stability to your crypto investments.</p>
+      <div className="h-screen flex items-center justify-center flex-col">
+        <div className="flex flex-col lg:items-center justify-center lg:w-[800px] text-center lg:space-y-14 space-y-6">
+          <h1 className="lg:text-6xl text-3xl text-blue-200 leading-tight px-4 lg:px-0">
+            Financial Stability Starts with Fixed Income Solutions
+          </h1>
+          <p className="lg:text-lg text-blue-100 px-4 lg:px-0">
+            Harness the power of funding rates and blockchain technology to
+            generate predictable, steady income streams. Our solutions are
+            designed to bring stability to your crypto investments.
+          </p>
+        </div>
+        <div className="w-full">
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center lg:space-x-6 space-y-6 lg:space-y-0 px-4 lg:px-0 mt-10">
+            <button className="lg:w-80 w-full bg-main text-black py-2 rounded-lg lg:text-lg text-md font-bold">
+              Get Started
+            </button>
+            <button className="lg:w-80 w-full bg-transparent border border-main text-blue-100 py-2 rounded-lg lg:text-lg text-md font-bold">
+              Learn More
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
